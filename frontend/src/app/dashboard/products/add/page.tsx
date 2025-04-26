f5b60228cbe07114bcd724773e1375765829ac51
aamirazaynn <102486232+aamirazaynn@users.noreply.github.com>
2025-04-26 03:39:11 +0300

    tabs in add product and product variation and options

--- frontend/src/app/dashboard/products/add/page.tsx
+++ frontend/src/app/dashboard/products/add/page.tsx
@@ -4,12 +4,17 @@ import React, { useState, useRef } from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Sidebar } from "@/components/sidebar/sidebar";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { ProductInfoSection } from "@/components/dashboard/products/add/productInfoSection";
 import { PricingSection } from "@/components/dashboard/products/add/pricingSection";
 import { LowStockSection } from "@/components/dashboard/products/add/lowStockSection";
+import { SizeVariationSection } from "@/components/dashboard/products/add/sizeVariationSection";
+import { ColorVariationSection } from "@/components/dashboard/products/add/colorVariationSection";
 
 export default function AddProductPage() {
+    const [activeTab, setActiveTab] = useState<"Product's Overview" | "Product's Options and Variations">("Product's Options and Variations");
+    const tabs = ["Product's Overview", "Product's Options and Variations"];
+
   return (
     <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
@@ -20,17 +25,49 @@ export default function AddProductPage() {
         <h1 className="text-2xl md:text-3xl font-bold mt-2">Add New Product</h1>
         <p className="text-gray-500 mt-2 mb-6">Create a new product for your store</p>
 
+        <div className="flex mb-1 ml-3">
+            {tabs.map((tab) => (
+                <button
+                    key={tab}
+                    onClick={() => setActiveTab(tab as typeof activeTab)}
+                    className={`px-4 py-2 border-b-2 transition-all duration-200 ${
+                    activeTab === tab ? 'border-logo-txt text-logo-txt font-medium' : 'border-transparent text-gray-500'
+                    }`}
+                >
+                    {tab}
+                </button>
+            ))}
+        </div>
+
         <Card className="flex flex-col gap-4 bg-white">
             <CardContent className="py-2">
             <form className="space-y-6">
-                {/* Product Info */}
-                <ProductInfoSection/>
+                {activeTab === "Product's Overview" && (
+                    <>
+                        {/* Product Info */}
+                        <ProductInfoSection/>
+                        
+                        {/* Pricing Section */}
+                        <PricingSection/>
+                        
+                        {/* Low Stock Settings Section */}
+                        <LowStockSection/>
+                    </>
+                )}
+
+                {activeTab === "Product's Options and Variations" && (
+                    <>
+                        <div className="mb-2">
+                            <CardTitle className="font-bold text-2xl">Product options and variations</CardTitle>
+                            <p className="text-gray-500">Add product options that your customers can choose from on the product page.</p>
+                        </div>
+
+                        <SizeVariationSection/>
+
+                        <ColorVariationSection/>
+                    </>
+                )}
                 
-                {/* Pricing Section */}
-                <PricingSection/>
-                
-                {/* Low Stock Settings Section */}
-                <LowStockSection/>
                 
                 {/* Form Actions */}
                 <div className="flex flex-col sm:flex-row gap-3 pt-4">
