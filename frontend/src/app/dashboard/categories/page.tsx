977a2a5ed4b1e795ab0f2edd86549414b28f8ea5
YehiaaZakaria <101659043+YehiaaZakaria@users.noreply.github.com>
2025-04-27 21:05:03 +0300

    Import and Export DropDown

--- frontend/src/app/dashboard/categories/page.tsx
+++ frontend/src/app/dashboard/categories/page.tsx
@@ -1,67 +1,65 @@
 // dropdown-menu.tsx
-"use client";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
-import { Sidebar } from "@/components/sidebar/sidebar";
-import Image from "next/image";
-import { categories } from "@/lib/categories";
-import { categoryAnalytics } from "@/lib/generalAnalytics";
-import { GeneralAnalyticsCard } from "@/components/dashboard/generalAnalyticsCard";
-import { CategoryRecord } from "@/components/dashboard/categories/categoryRecord";
-import { SearchBar } from "@/components/ui/searchBar";
-import { CategoryTableHeader } from "@/components/dashboard/categories/categoryTableHeader";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from "@/components/ui/dropdown-menu";
-import { useState } from "react";
+"use client"
+import Link from "next/link"
+import type React from "react"
+
+import { Button } from "@/components/ui/button"
+import { Sidebar } from "@/components/sidebar/sidebar"
+import Image from "next/image"
+import { categories } from "@/lib/categories"
+import { categoryAnalytics } from "@/lib/generalAnalytics"
+import { GeneralAnalyticsCard } from "@/components/dashboard/generalAnalyticsCard"
+import { CategoryRecord } from "@/components/dashboard/categories/categoryRecord"
+import { SearchBar } from "@/components/ui/searchBar"
+import { CategoryTableHeader } from "@/components/dashboard/categories/categoryTableHeader"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu"
+import { useState } from "react"
+
 export default function CategoriesPage() {
   const [sortType, setSortType] = useState<
-    | "newest"
-    | "oldest"
-    | "nameAsc"
-    | "nameDesc"
-    | "productsAsc"
-    | "productsDesc"
-  >("newest");
+    "newest" | "oldest" | "nameAsc" | "nameDesc" | "productsAsc" | "productsDesc"
+  >("newest")
+  const [file, setFile] = useState<File | null>(null)
 
-  const handleSortChange = (
-    type:
-      | "newest"
-      | "oldest"
-      | "nameAsc"
-      | "nameDesc"
-      | "productsAsc"
-      | "productsDesc"
-  ) => {
-    setSortType(type);
-  };
+  const handleSortChange = (type: "newest" | "oldest" | "nameAsc" | "nameDesc" | "productsAsc" | "productsDesc") => {
+    setSortType(type)
+  }
 
-  // sort categories based on the selected type
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0]
+    if (selectedFile) {
+      setFile(selectedFile)
+      // You can add additional logic here to process the file
+      console.log("File selected:", selectedFile.name)
+
+      // Optional: You could add file reading logic here
+      // const reader = new FileReader();
+      // reader.onload = (e) => {
+      //   const content = e.target?.result;
+      //   // Process the file content
+      // };
+      // reader.readAsText(selectedFile);
+    }
+  }
+
   const sortedCategories = categories.sort((a, b) => {
     switch (sortType) {
       case "newest":
-        return (
-          new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-        );
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
       case "oldest":
-        return (
-          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-        );
+        return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
       case "nameAsc":
-        return a.title.localeCompare(b.title); // Sort by name A to Z
+        return a.title.localeCompare(b.title) // Sort by name A to Z
       case "nameDesc":
-        return b.title.localeCompare(a.title); // Sort by name Z to A
+        return b.title.localeCompare(a.title) // Sort by name Z to A
       case "productsAsc":
-        return a.numOfProducts - b.numOfProducts; // Sort by number of products ascending
+        return a.numOfProducts - b.numOfProducts // Sort by number of products ascending
       case "productsDesc":
-        return b.numOfProducts - a.numOfProducts; // Sort by number of products descending
+        return b.numOfProducts - a.numOfProducts // Sort by number of products descending
       default:
-        return 0;
+        return 0
     }
-  });
+  })
 
   return (
     <div className="flex min-h-screen bg-gray-100">
@@ -73,34 +71,39 @@ export default function CategoriesPage() {
 
         {/* Header section */}
         <div className="flex flex-col md:flex-row md:justify-between md:items-center mt-4 gap-4">
-          <h2 className="text-lg md:text-xl font-semibold">
-            Manage your product categories and organization
-          </h2>
+          <h2 className="text-lg md:text-xl font-semibold">Manage your product categories and organization</h2>
           <div className="flex flex-wrap gap-2 md:flex-col lg:flex-row md:items-center justify-end">
             <Link href="/dashboard/categories/add" className="w-full sm:w-auto">
               <Button className="w-full sm:w-auto bg-logo-dark-button text-primary-foreground hover:bg-logo-dark-button-hover">
-                <Image
-                  src="/icons/plus.svg"
-                  alt="Add Icon"
-                  width={20}
-                  height={20}
-                />
+                <Image src="/icons/plus.svg" alt="Add Icon" width={20} height={20} />
                 <span className="ml-2">Add New Category</span>
               </Button>
             </Link>
-            <Button
-              variant="outline"
-              size="default"
-              className="w-full sm:w-auto text-logo-txt hover:text-logo-txt-hover hover:bg-logo-light-button-hover border-logo-border"
-            >
-              <Image
-                src="/icons/dropdown-colored.svg"
-                alt="Dropdown Icon"
-                width={20}
-                height={20}
-              />
-              <span className="ml-2">Import or Export</span>
-            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="w-full sm:w-auto text-logo-txt hover:text-logo-txt-hover hover:bg-logo-light-button-hover border-logo-border"
+                >
+                  <span className="ml-2">Import or Export Categories</span>
+                  <Image src="/icons/dropdown-colored.svg" alt="Dropdown Icon" width={20} height={20} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <label htmlFor="import-file">Import Categories From Excel Sheet</label>
+                  <input
+                    id="import-file"
+                    type="file"
+                    accept=".xlsx,.xls,.csv"
+                    onChange={handleFileChange}
+                    style={{ display: "none" }}
+                  />
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => console.log(file)}>Export All Categories</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </div>
 
@@ -128,47 +131,26 @@ export default function CategoriesPage() {
                     {sortType === "newest"
                       ? "Newest"
                       : sortType === "oldest"
-                      ? "Oldest"
-                      : sortType === "nameAsc"
-                      ? "Name (A to Z)"
-                      : sortType === "nameDesc"
-                      ? "Name (Z to A)"
-                      : sortType === "productsAsc"
-                      ? "Products (Asc)"
-                      : "Products (Desc)"}
+                        ? "Oldest"
+                        : sortType === "nameAsc"
+                          ? "Name (A to Z)"
+                          : sortType === "nameDesc"
+                            ? "Name (Z to A)"
+                            : sortType === "productsAsc"
+                              ? "Products (Asc)"
+                              : "Products (Desc)"}
                   </span>
 
-                  <Image
-                    src="/icons/dropdown-colored.svg"
-                    alt="Dropdown Icon"
-                    width={20}
-                    height={20}
-                  />
+                  <Image src="/icons/dropdown-colored.svg" alt="Dropdown Icon" width={20} height={20} />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuItem onClick={() => handleSortChange("newest")}>
-                  Newest
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleSortChange("oldest")}>
-                  Oldest
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleSortChange("nameAsc")}>
-                  Name (A to Z)
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => handleSortChange("nameDesc")}>
-                  Name (Z to A)
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => handleSortChange("productsAsc")}
-                >
-                  Products (Asc)
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onClick={() => handleSortChange("productsDesc")}
-                >
-                  Products (Desc)
-                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("newest")}>Newest</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("oldest")}>Oldest</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("nameAsc")}>Name (A to Z)</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("nameDesc")}>Name (Z to A)</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("productsAsc")}>Products (Asc)</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => handleSortChange("productsDesc")}>Products (Desc)</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
@@ -188,5 +170,5 @@ export default function CategoriesPage() {
         </div>
       </main>
     </div>
-  );
+  )
 }
