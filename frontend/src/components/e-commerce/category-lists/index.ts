53b2fe8b6507f89246d6e53f87c832a6b807999c
amnayahia <100522656+26amna@users.noreply.github.com>
2025-06-30 22:42:08 +0300

    removed list-view-template from the category-list

--- frontend/src/components/e-commerce/category-lists/index.ts
+++ frontend/src/components/e-commerce/category-lists/index.ts
@@ -1,6 +1,5 @@
 import { GridCategoryTemplate } from "./templates/grid-template";
 import FeaturedGridCategoryTemplate from "./templates/featured-grid-template";
 import { HorizontalScrollCategoryTemplate } from "./templates/horizontal-scroll-template";
-import { ListViewCategoryTemplate } from "./templates/list-view-template";
-export { GridCategoryTemplate, FeaturedGridCategoryTemplate, HorizontalScrollCategoryTemplate, ListViewCategoryTemplate };
+export { GridCategoryTemplate, FeaturedGridCategoryTemplate, HorizontalScrollCategoryTemplate };
 
